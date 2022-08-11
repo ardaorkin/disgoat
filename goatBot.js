1b98b8afe4945ae75aea4f5807032abe52f19aa4
Arda Örkin <ardaorkin@gmail.com>
2022-08-12 01:37:56 +0300

    Initial commit

---
+++ goatBot.js
@@ -0,0 +1,32 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const { searchGoats } = require("./searchGoats");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+async function embedGoats() {
+  try {
+    const images = await searchGoats();
+    const randomId = Math.floor(Math.random() * 100);
+    const embed = new EmbedBuilder()
+      .setTitle("Lovely Goats")
+      .setImage(images[0].thumbnailLink);
+    return client.channels.cache
+      .get("1007408136778428459")
+      .send({ embeds: [embed] });
+  } catch (error) {
+    return client.channels.cache.get("1007408136778428459").send(error.message);
+  }
+}
+
+// When the client is ready, run this code (only once)
+client.once("ready", async () => {
+  await embedGoats();
+  setInterval(async () => await embedGoats(), 1000 * 60 * 60);
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ searchGoats.js
@@ -0,0 +1,21 @@
+var fetch = require("node-fetch");
+var dotenv = require("dotenv");
+dotenv.config();
+
+async function searchGoats() {
+  const images = [];
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=goat&searchType=image&imgSize=huge&sort=date`
+    );
+    const result = await response.json();
+    const { items } = result;
+    items.map((item) => {
+      images.push(item.image);
+    });
+    return images;
+  } catch (error) {
+    throw error;
+  }
+}
+module.exports = { searchGoats };
