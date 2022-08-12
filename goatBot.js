809efe811d1498d44f83cdd0cc37858a64d94f68
Arda Örkin <ardaorkin@gmail.com>
2022-08-13 00:32:16 +0300

    cron

--- goatBot.js
+++ goatBot.js
@@ -1,36 +1,14 @@
 // Require the necessary discord.js classes
 const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 var cron = require("node-cron");
-const sharp = require("sharp");
-const fetch = require("node-fetch");
-const fs = require("fs");
 const { searchGoats } = require("./searchGoats");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const {
-  DISCORD_CHANNEL_ID,
-  DISCORD_BOT_TOKEN,
-  EMBED_IMAGE_URL,
-  EMBED_IMAGE_FILE_NAME,
-} = process.env;
+const { DISCORD_CHANNEL_ID, DISCORD_BOT_TOKEN } = process.env;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-async function convertAndSaveImg(url) {
-  try {
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-    const binary = Buffer.from(arrayBuffer, "binary");
-    if (!fs.existsSync(EMBED_IMAGE_FILE_NAME)) {
-      fs.writeFile(EMBED_IMAGE_FILE_NAME, "", () => {});
-    }
-    sharp(binary).resize(350, 260).toFile(EMBED_IMAGE_FILE_NAME);
-  } catch (error) {
-    throw error;
-  }
-}
 
 async function embedGoats() {
   try {
@@ -38,14 +16,11 @@ async function embedGoats() {
     const randomId = Math.floor(
       parseInt(new Date().toLocaleTimeString("tr-Tr", { hour: "numeric" })) / 4
     );
-    await convertAndSaveImg(images[randomId].thumbnailLink);
-
     const embed = new EmbedBuilder()
       .setColor(0x0099ff)
       .setTitle("Lovely Goats")
-      .setImage(EMBED_IMAGE_URL + EMBED_IMAGE_FILE_NAME)
-      .setTimestamp();
-
+      .setTimestamp()
+      .setImage(images[randomId].thumbnailLink);
     return client.channels.cache
       .get(DISCORD_CHANNEL_ID)
       .send({ embeds: [embed] });
