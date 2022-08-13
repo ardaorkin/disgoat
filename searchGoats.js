b7715e784c61b17acd1e781e2c4c6f855a8670b4
Arda Örkin <ardaorkin@gmail.com>
2022-08-13 17:07:18 +0300

    sort search by date

--- searchGoats.js
+++ searchGoats.js
@@ -5,7 +5,7 @@ dotenv.config();
 async function searchGoats() {
   try {
     const response = await fetch(
-      `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=goat&searchType=image`
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=goat&searchType=image&sort=date`
     );
     const result = await response.json();
     const { items } = result;
