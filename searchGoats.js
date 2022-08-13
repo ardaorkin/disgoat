8504715826b2f46a480f850f0e16ad67e43820c8
Arda Örkin <ardaorkin@gmail.com>
2022-08-13 17:08:34 +0300

    code convenient

--- searchGoats.js
+++ searchGoats.js
@@ -2,10 +2,12 @@ var fetch = require("node-fetch");
 var dotenv = require("dotenv");
 dotenv.config();
 
+const { GOOGLE_SEARCH_KEY, SEARCH_ENGINE_ID } = process.env;
+
 async function searchGoats() {
   try {
     const response = await fetch(
-      `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=goat&searchType=image&sort=date`
+      `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_SEARCH_KEY}&cx=${SEARCH_ENGINE_ID}&q=goat&searchType=image&sort=date`
     );
     const result = await response.json();
     const { items } = result;
