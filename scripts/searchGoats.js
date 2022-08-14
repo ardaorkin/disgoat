1ff4c03e7666635643bdea0eed8680db5a847885
Arda Örkin <ardaorkin@gmail.com>
2022-08-14 11:55:37 +0300

    add google search engine start index

--- scripts/searchGoats.js
+++ scripts/searchGoats.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 const { GOOGLE_SEARCH_KEY, SEARCH_ENGINE_ID } = process.env;
 
-async function searchGoats() {
+async function searchGoats(startIndex) {
   try {
     const response = await fetch(
-      `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_SEARCH_KEY}&cx=${SEARCH_ENGINE_ID}&q=goat&searchType=image&sort=date`
+      `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_SEARCH_KEY}&cx=${SEARCH_ENGINE_ID}&q=goat&searchType=image&sort=date&start=${startIndex}`
     );
     const result = await response.json();
     const { items } = result;
